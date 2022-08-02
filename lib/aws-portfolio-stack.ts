e13a5b559dd9398ea5463fe6e1138b72a9eafeb2
sasha <sasha.luhr@gmail.com>
2022-08-02 12:25:19 +0200

    build steps

--- lib/aws-portfolio-stack.ts
+++ lib/aws-portfolio-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { addStaticPageBucket } from './s3resources';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsPortfolioStack extends Stack {
@@ -14,7 +15,8 @@ export class AwsPortfolioStack extends Stack {
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('sashinshin/aws-portfolio', 'main'),
         commands: [
-          'cd /frontend',
+          'ls',
+          `cd ${join(__dirname, "../frontend/build")}`,
           'npm ci',
           'npm run build',
           'cd ..',
